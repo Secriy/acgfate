476b67e0463e592302a08de2d07e114331f36cd3
Secriy <secriyal@gmail.com>
2021-10-11 15:47:01 +0800

    docs: change error message

--- cache/cache.go
+++ cache/cache.go
@@ -14,7 +14,7 @@ var RDB *redis.Client
 
 // InitRedisClient 在中间件中初始化redis链接
 func InitRedisClient() {
-	conf := config.Conf.Redis // 配置
+	conf := config.Conf.RedisConf
 	RDB = redis.NewClient(&redis.Options{
 		Addr:     conf.Host,
 		Password: conf.Password,
@@ -26,6 +26,6 @@ func InitRedisClient() {
 	_, err := RDB.Ping(ctx).Result()
 
 	if err != nil {
-		panic(fmt.Errorf("连接Redis不成功: %s \n", err))
+		panic(fmt.Errorf("connect to Redis failed: %s \n", err))
 	}
 }
